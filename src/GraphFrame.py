c8dba4d54bfa53864809ff4064fd656d0cbc5ab9
HelenYunes <74502978+HelenYunes@users.noreply.github.com>
2021-01-10 00:44:31 +0200

    Fixed GraphFrame

--- src/GraphFrame.py
+++ src/GraphFrame.py
@@ -6,32 +6,31 @@ import random
 class GraphFrame:
     def __init__(self, graph: DiGraph):
         self.graph = graph
-        self.list_random_location_nodes = []
-        self.set_random_nodes()
 
     def set_random_nodes(self):
-        eps = 0.0002
+        list_random_location_nodes = []
         for node in self.graph.get_all_v().values():
             if node.get_location() is None:
-                self.list_random_location_nodes.append(node)
-        if self.list_random_location_nodes.__len__() == 0:
+                list_random_location_nodes.append(node)
+        if list_random_location_nodes.__len__() == 0:
             return
-        for node in self.list_random_location_nodes:
+        for node in list_random_location_nodes:
             if node.get_location() is None:
                 node.set_location(random.uniform(0, 7), random.uniform(0, 7), 0)
 
     def draw_nodes(self, ax):
+        self.set_random_nodes()
         for node in self.graph.Nodes.values():
-            ax.scatter(node.get_location()[0], node.get_location()[1], color="blue", s=50, label=node.__repr__())
+            ax.scatter(node.get_location()[0], node.get_location()[1], color="blue", label=node.__repr__())
 
     def draw_edges(self, ax):
+        eps = 0.0001
         for node in self.graph.Nodes.values():
             for edge in self.graph.all_out_edges_of_node(node.get_key()).keys():
-                destination = self.graph.get_node(edge)
                 ax.arrow(node.get_location()[0], node.get_location()[1],
-                         destination.get_location()[0] - node.get_location()[0],
-                         destination.get_location()[1] - node.get_location()[1], label='Edges',
-                         length_includes_head=True, head_width=0.025, head_length=0.18)
+                         self.graph.get_node(edge).get_location()[0] - node.get_location()[0],
+                         self.graph.get_node(edge).get_location()[1] - node.get_location()[1], label='Edges',
+                         length_includes_head=True, width=0.00000389, head_width=0.000089999, head_length=0.000299974)
 
     def draw_graph(self):
 
@@ -40,7 +39,7 @@ class GraphFrame:
         plot_graph.title("Here is a graphic presentation of the graph:", color="w")
         plot_graph.xlabel("x")
         plot_graph.ylabel("y")
-        self.draw_edges(ax)
         self.draw_nodes(ax)
+        self.draw_edges(ax)
         plot_graph.tight_layout()
         plot_graph.show()
